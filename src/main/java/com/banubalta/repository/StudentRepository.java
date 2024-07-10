66790d0681910d26306afa378a47127dcbc2b4cc
Banuubalta <nbanu.balta@gmail.com>
2024-07-11 02:26:31 +0300

    first commit

---
+++ src/main/java/com/banubalta/repository/StudentRepository.java
@@ -0,0 +1,7 @@
+package com.banubalta.repository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class StudentRepository {
+}
---
+++ src/main/java/com/banubalta/service/StudentService.java
@@ -0,0 +1,8 @@
+package com.banubalta.service;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+}
